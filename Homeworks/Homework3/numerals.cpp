bc0d119999f856d816c03b875ec73ff7b1ba6f1a
Bucky Frost <frozenouthere@gmail.com>
2014-03-01 01:20:03 -0900

    Removed the main function for the Catch framework main macro.

--- Homeworks/Homework3/numerals.cpp
+++ Homeworks/Homework3/numerals.cpp
@@ -4,10 +4,6 @@
  * Purpose: Implementation file for numbers to roman numerals kata.
  * 	    For CS 372 Homework 3.
  */
+#define CATCH_CONFIG_MAIN
 
 #include "catch.hpp"
-
-int main()
-{
-	return 0;
-}
